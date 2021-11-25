7854203dc8e10345ecc1d85ca183a151ba4c7695
webbravo <4natcom@gmil.com>
2021-11-24 19:31:22 -0800

    Second SQL task

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- A script that creates the database hbtn_0c_0 in your MySQL server.
+IF NOT EXISTS CREATE TABLE `hbtn_0c_0`
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Delete a table (Execute without an exception thrown)
+IF NOT EXISTS DELETE TABLE `hbtn_0c_0`
